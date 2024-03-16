5a471e6cc65acf3eb045de64743b91ba59eb5002
wushangyuan <wwilliamkairos@gmail.com>
2024-03-16 23:09:49 +0800

    initialization

---
+++ src/main/java/com/itcoursedesign/ItCourseDesignApplication.java
@@ -0,0 +1,13 @@
+package com.itcoursedesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItCourseDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItCourseDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itcoursedesign/ItCourseDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcoursedesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItCourseDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
