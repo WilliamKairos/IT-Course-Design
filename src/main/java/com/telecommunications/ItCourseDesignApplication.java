6f57022f8424b7521fe3dd8ab736a2591ff19718
wushangyuan <wwilliamkairos@gmail.com>
2024-03-19 21:24:14 +0800

    完成工具类的设置

--- src/main/java/com/telecommunications/ItCourseDesignApplication.java
+++ src/main/java/com/telecommunications/ItCourseDesignApplication.java
@@ -1,4 +1,4 @@
-package com.itcoursedesign;
+package com.telecommunications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/telecommunications/ItCourseDesignApplicationTests.java
+++ src/test/java/com/telecommunications/ItCourseDesignApplicationTests.java
@@ -1,4 +1,4 @@
-package com.itcoursedesign;
+package com.telecommunications;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
